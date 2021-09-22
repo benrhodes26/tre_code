9be008fb4002a0f8795ca773d05bf4e2f863a358
Seungwook Han <sh3264@coumbia.edu>
2021-09-22 17:45:35 -0400

    Redirecting both stdout & stderr

--- run_gaussians_tre.sh
+++ run_gaussians_tre.sh
@@ -4,6 +4,6 @@ for c in 0 2 4
 do
     for i in 1 2
     do
-        CUDA_VISIBLE_DEVICES=1 python build_bridges.py --config_path=gaussians/model/${c}_${i} > ${c}_run${i}.txt
+        CUDA_VISIBLE_DEVICES=1 python build_bridges.py --config_path=gaussians/model/${c}_${i} > ${c}_run${i}.txt 2>&1
     done
 done
