2509f4b7bbf92e8d878603cf1dbb47c66ae48b32
Seungwook Han <sh3264@coumbia.edu>
2021-09-22 17:38:29 -0400

    Fixed run scripts

--- run_gaussians_single_ratio.sh
+++ run_gaussians_single_ratio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for c in 0 2 4
+for c in 1 3 5
 do
-    for i in 0 1 2
+    for i in 1 2 3
     do
         CUDA_VISIBLE_DEVICES=1 python build_bridges.py --config_path=gaussians/model/${c}_${i} > ${c}_run${i}.txt
--- run_gaussians_tre.sh
+++ run_gaussians_tre.sh
@@ -2,6 +2,6 @@
 
 for c in 0 2 4
 do
-    for i in 0 1 2
+    for i in 1 2 3
     do
         CUDA_VISIBLE_DEVICES=1 python build_bridges.py --config_path=gaussians/model/${c}_${i} > ${c}_run${i}.txt
